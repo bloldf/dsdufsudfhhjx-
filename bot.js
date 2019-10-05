a286bbe13e897f7fda5b4f7fdaa4cc3e03008db6
bloldf <48572660+bloldf@users.noreply.github.com>
2019-10-05 19:10:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1042,7 +1042,6 @@ if (message.author.omar) return;
 if (!message.content.startsWith(prefix)) return;
 if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
 if(!message.member.hasPermission('MANAGE_ROLES')) return
-if(!message.guild.member(client.user).hasPermission("MANAGE_ROLES")) return message.reply("**I Don't Have `MANAGE_ROLES` Permission**").then(msg => msg.delete(6000))
 var command = message.content.split(" ")[0];
 command = command.slice(prefix.length);
 var args = message.content.split(" ").slice(1);
